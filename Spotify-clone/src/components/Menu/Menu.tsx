0cbf054e9539ab136adb5ed0e63641c4e24e69af
ducanhaovai <ducanhaovai@users.noreply.github.com>
2024-07-11 19:23:48 +0700

    update

--- Spotify-clone/src/components/Menu/Menu.tsx
+++ Spotify-clone/src/components/Menu/Menu.tsx
@@ -60,8 +60,8 @@ const Menu = ({ isOpen, onClose }: Props) => {
             <path
               d="M18 18L12 12M12 12L6 6M12 12L18 6M12 12L6 18"
               stroke="#A9A9A9"
-              stroke-width="2"
-              stroke-linecap="round"
+              strokeWidth="2"
+              strokeLinecap="round"
               stroke-linejoin="round"
             />
           </svg>
